9b5cb9d53895b5a3703f31b95da4c84f72d1ee8e
Guy <premsupapong@gmail.com>
2020-06-28 12:18:58 +0700

    modified for testing

--- gallery.cpp
+++ gallery.cpp
@@ -40,68 +40,13 @@ int main(int argc, char** args){
         return -1;
     }
 
-    // initalise the required environments
-    if(!init()){
-        cerr << "Error: Failed initalisiation. " << endl;
-    }else{
-
-        // get all image file names in specified path
-        std::unique_ptr<PathVector> imgDirs = getImgDirs(path);
-
-        // try printing the vector of dirs
-        for(const auto &path : *imgDirs){
-            cout << path << endl;
-        }
-
-    }
+    Window w = Window();
+    cout << w.get_window() << endl;
 
     return 0;
 
 }
 
-bool init(){
-    // initalise SDL, SDL_IMG + other environments
-    bool success = true;
-
-    // initalise SDL environment
-    if(SDL_Init(SDL_INIT_VIDEO)){
-        cerr << "Error: Failed SDL Initalisiation. " << SDL_GetError() << endl;
-        success = false;
-    }else{
-
-        cout << "Successful SDL Initalisation" << endl;
-
-        // initalise SDL window
-        g_Window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if(g_Window == NULL){
-            cerr << "Error: SDL Window cannot be created. " << SDL_GetError() << endl;
-            success = false;
-        }else{
-
-            // initalise SDL renderer
-            g_Renderer = SDL_CreateRenderer(g_Window, -1, SDL_RENDERER_ACCELERATED);
-            if(g_Renderer == NULL){
-                cout << "Error: Renderer failed initliasiation. " << SDL_GetError() << endl;
-                success = false;
-            }else{
-
-                // set colour of renderer initialsation
-                SDL_SetRenderDrawColor(g_Renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                // initalise SDL_IMG environment
-                // -> set up for loading JPG + PNG images
-                int imgFlags = IMG_INIT_JPG | IMG_INIT_PNG;
-                if( (IMG_Init(imgFlags)&imgFlags) != imgFlags){
-                    cout << "Error: SDL_image failed initialization. " << IMG_GetError() << endl;
-                    success = false;
-                }
-            }
-        }
-    }
-
-    return success;
-
-}
 
 std::unique_ptr<PathVector> getImgDirs(std::string &path){
     // returns smart pointer to names of all images in path directory
