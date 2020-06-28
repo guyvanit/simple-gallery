9796bc515af0d1042f1c1bac0ca89339fbc7b6fa
Guy <premsupapong@gmail.com>
2020-06-28 12:18:18 +0700

    getter and naming convention

--- wrappers/sdl_window.cpp
+++ wrappers/sdl_window.cpp
@@ -1,16 +1,23 @@
 #include "sdl_window.h"
 #include "sdl_exceptions.h"
 
-Window::Window(const std::string &title="SDL test", int x=SDL_WINDOWPOS_UNDEFINED, int y=SDL_WINDOWPOS_UNDEFINED,
-                int w=640, int h=480, Uint32 flags=SDL_WINDOW_SHOWN){
+Window::Window(const std::string &title, int x, int y, int w, int h, Uint32 flags){
 
     // initalise SDL Window object and test for NULL
-    win = SDL_CreateWindow(title.c_str(), x, y, w, h, flags);
-    if(win==NULL){
+    win_ = SDL_CreateWindow(title.c_str(), x, y, w, h, flags);
+    if(win_==NULL){
         // throw Exception("Window class constructor");
         throw;
     }
 
+}
+
+Window::~Window(){
+    SDL_DestroyWindow(win_);
+}
+
+SDL_Window* Window::get_window(){
+    return win_;
 }
 
 
