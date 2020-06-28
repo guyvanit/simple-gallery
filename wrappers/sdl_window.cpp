f1ed509ebcb2065a7a50103e786d25340a454042
Guy <premsupapong@gmail.com>
2020-06-28 12:54:10 +0700

    implemented Exception class

--- wrappers/sdl_window.cpp
+++ wrappers/sdl_window.cpp
@@ -6,8 +6,7 @@ Window::Window(const std::string &title, int x, int y, int w, int h, Uint32 flag
     // initalise SDL Window object and test for NULL
     win_ = SDL_CreateWindow(title.c_str(), x, y, w, h, flags);
     if(win_==NULL){
-        // throw Exception("Window class constructor");
-        throw;
+        throw Exception("Window::Window()");
     }
 
 }
