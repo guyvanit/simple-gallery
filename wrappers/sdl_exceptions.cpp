6c9dfd942c47b3ae0494c0f44707dfa3ee7cdbef
Guy <premsupapong@gmail.com>
2020-06-28 12:53:40 +0700

    getters for exception details

--- wrappers/sdl_exceptions.cpp
+++ wrappers/sdl_exceptions.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
 #include "sdl_exceptions.h"
 
-Exception::Exception(std::string fnc){
-    err_msg = "ERR: " + SDL_GetError() + ", from " + fnc;
+Exception::Exception(std::string f){
+    fnc_ = f;
+    sdl_err_ = SDL_GetError();
 }
+
+std::string Exception::get_err(){
+    return sdl_err_;
+}
+
+std::string Exception::get_fnc(){
+    return fnc_;
+}
+
--- wrappers/sdl_exceptions.h
+++ wrappers/sdl_exceptions.h
@@ -1,11 +1,17 @@
-#include <SDL2/SDL.h>
 #include <string>
 
 class Exception{
-    std::string err_msg;
+    std::string sdl_err_;
+    // stores which function causes the exception to be thrown
+    std::string fnc_;
 
     public:
-        Exception(std::string fnc);
-        ~Exception();
+        // constructor + (default) destructor?
+        Exception(std::string f);
+
+        // getters -> would storing as char[] use less memory/better?
+        std::string get_err();
+        std::string get_fnc();
+
 
 };
